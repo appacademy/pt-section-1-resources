300b23777193385d9f291f6e8269f55bfdabb5c8
William Vincent <william@william-vincent.dev>
2023-10-17 23:01:44 -0400

    scratch file update

---
+++ 01-week/2-day/scratch.js
@@ -0,0 +1,166 @@
+ 
+
+// // let myVar = 5;
+// // let myVar = 5; -- a value **is** something
+// // an expression is a statement that resolves to a value.
+// // console.log(5+5) // 5 + 5  -- resolves to something
+// // console.log(5 === 5); // true
+
+
+// // 7
+
+// // 7.0
+
+// // -7
+
+// 5 * 3 + 7 / 2
+
+// // P.E.(M.M.D).(A.S)
+
+// /*
+// Parens
+
+// Exponents
+
+// Multiplication / Division / Modulus
+
+// Addition / Subtraction
+
+// If a match resolve left to right
+// */
+
+// // Modulo is the remainder operator
+
+// // 10 % 2
+
+
+
+// // Not (!) -- bang 
+
+// // And (&&)
+
+// // Or (||)
+
+// // equality operators
+
+// let val = 5; // assignment
+// // = -- this is not checking for equality -- this is making a statement of fact
+// // = assignment operator
+// // == -- loose equality // coerces data types -- unexpected behavior
+// // === -- strict equality
+
+// // = -- assignment
+
+// // left -- namespace -- a labeled box to put info in
+// // right -- the value -- the thing to put in the box
+
+
+// //declaration
+// let namespace;
+
+// //initialization
+// let initialize = "a value";
+
+// // assigned
+// namespace = 5;
+
+// // reassigned
+// namespace = 7
+
+
+
+// let num = 0;
+
+// // reassignment combination operators
+
+// num = num + 2; // ? -> 2
+
+// num += 2;
+
+// num -= 2;
+
+// num *= 2;
+
+// num /= 2;
+
+
+// // prefix and postfix
+
+// num++ // num = num + 1 -- postfix
+// ++num // num = num + 1 -- prefix
+
+// num--
+// --num
+
+
+
+// ------- PST ----------------------
+
+//statement // value is something
+// let myVar = 5;
+
+// expression evaluates to something
+// 5 + 5 // -> 10
+
+// 5 === 5 // true
+
+
+// Number: 7, 7.0, -7
+
+
+// Order or Operations
+
+/*
+P. Parens
+E. Exponent
+M.M.D. Multiplication, Modulus (Remainder), Division
+A.S. Addition, Subtraction
+*/
+
+
+// 5 * 3 + 7 / 2
+// 15 + 3.5
+// 18.5
+
+
+// 5 + 'S'
+
+// = : assignment operator : does not check equality
+
+// == :  loose equality operator : attempts to coerce data to check equality
+
+// === : strict equality : does not attempt to coerce data type
+
+let name = 'William';
+
+// declaration 
+let namespace;
+
+// initializing
+let age = 20;
+
+//assignment
+namespace = 5;
+
+//reassignment
+namespace = 10;
+
+// name = 5;
+
+// console.log(name);
+
+
+let num = 0;
+
+num = num + 1;
+num += 1;
+
+num -= 1; // num = num - 1;
+num *= 1; // num = num * 1;
+num /= 1;
+
+num++
+++num
+
+num--
+--num
