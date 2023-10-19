21e903cef9e70afb4030cd845a592dfb81962351
William Vincent <william@william-vincent.dev>
2023-10-19 14:24:55 -0400

    scratch update

---
+++ 01-week/3-day/scratch.js
@@ -0,0 +1,179 @@
+// let myStrOne = 'a string'
+// let myStrTwo = "a different string"
+
+// // let str = 'William's Computer';
+// let str2 = "William's Computer";
+// let str3 = 'William\'s Computer';
+
+// // let example = "cat";
+
+// // console.log(example.length);
+// // console.log(str3);
+
+// /*
+// 0 | 1 | 2
+// c | a | t -> length 3
+// */
+
+// // console.log(example[2]);
+
+
+// let username = 'wvincent';
+
+// // console.log(username[username.length - 1]);
+// // console.log(username.indexOf('z'));
+
+// // function is a block of code that you can run whenever you want to
+
+// // console.log(2+2)
+// // console.log(2+4)
+// // console.log(2+6)
+// // console.log(2+7)
+// // console.log(2+8)
+// // console.log(2+9)
+// // console.log(2+1)
+// // console.log(2+2)
+// // console.log(2+4)
+
+// // function add(a, b){
+// //     return a + b;
+// // };
+
+// // add(2,2)
+// // add(8,28)
+
+// let a = 5;
+
+
+// //function declaration
+// function add(a, b){ //parameters - potential values
+//     return a + b;
+// };
+
+
+// //invoking // calling
+// add(4,4);// -> 8  // arguments - actual values
+
+// //blueprinting
+// function aName (/*params go here*/) {
+//     //logic goes here
+// };
+
+// // function name () {
+
+// // }
+
+
+// // return vs console.log()
+
+// //side effect
+// // console.log(5)
+
+
+// function timesTwo(num1){
+//     return num1 * 2;
+// };
+
+// // let res = timesTwo(2);
+
+// // console.log(res) // -> 4
+
+// // function timesTwoAgain(num1){
+
+// // }; // return undefined
+
+
+// let count = 0;
+
+// function increaseCount(amount){
+//     count += amount;
+// };
+
+
+// // console.log(count); // 0
+// // increaseCount(5);
+// // console.log(count); // 5
+
+// // 0 1 2 3 4
+// // b o o t c 
+
+
+// // 0 1 2 3 4
+// // r u n 
+
+// ------- PST ----------
+
+// console.log('hello')
+
+
+// char === 'a'
+// string === 'aaaaaaaa'
+
+
+let str1 = 'world';
+let str2 = "hello";
+
+
+// let str3 = 'William's Computer';
+let str4 = "William's Computer";
+let str3 = 'William\'s Computer';
+
+
+// console.log(str3)
+
+let myVar = 'Cat';
+
+// console.log(myVar.length);
+
+
+// 0 | 1 | 2
+// C | a | t
+
+// console.log(myVar[4]);
+
+
+// console.log(str4.length)
+
+let crazyString = '\'\'\'\'\'';
+let otherString = "'\"\'\'\"'";
+
+// console.log(crazyString);
+
+let username = "wvincent";
+// console.log(username[username.length - 1]);
+
+// console.log(username.indexOf('cent'));
+
+let a = 'hello';
+let b = 'world';
+
+// console.log(a + " " + b);
+
+// reusable - portable - block of code
+
+// declaration - one time
+function timesTwo(num){ // parameters - potential values
+    return num * 2;
+};
+
+// call / invoke
+timesTwo(2); // -> 4 // argument - actual value
+
+
+function test(){
+    return 1
+};
+
+// console.log(timesTwo(4));
+
+// function concatStr(str1, str2){
+//     return str1 + str2;
+// };
+
+function concatStr(str1, str2){
+
+};
+
+let word = 'Hello'
+
+console.log(word.toUpperCase + " ... " + word + " ... " + word.toLowerCase)
