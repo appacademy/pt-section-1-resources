6e7a8bfaefb549110f93a7155dc8348837d51a3b
William Vincent <william@william-vincent.dev>
2023-10-20 00:50:09 -0400

    scratch update

---
+++ 01-week/4-day/scratch.js
@@ -0,0 +1,184 @@
+// AND OR NOT
+
+// let myName = 'Kate'
+
+// if(myName !== 'Kate'){
+//     console.log('I am not Kate')
+// };
+
+// let myNum = 5
+
+// if(myNum % 2 === 0){
+//     console.log('myNum is even')
+// } else if(myNum === 0){
+//     console.log('myNum is zero')
+// } else if(myNum === NaN){
+//     console.log('myNum isnt a num')
+// } else {
+//     console.log('myNum is odd')
+// };
+
+
+// let myName = 'William';
+//let i = 0; // i is not special -> it is not a keyword -> it is just a variable
+
+// while(i < myName.length){
+//     console.log(i + ": " + myName[i]);
+//     i = i + 1;
+// };
+
+// some condition that stops the iteration / loop
+// step which moves us closer to the condition that stops the loop 
+// (unless we do want an infinite loop)
+
+
+// while(true){
+
+// }
+
+
+// for (start, stop, step)
+
+// for( i=0; ) do not do this
+// for(let i = 0; i < myName.length; i = i + 1){
+//     let ele = myName[i]; 
+//     console.log(ele);
+// };
+
+// When to use?
+
+// For Loop: Use when a loops duration is based on a predefined length
+// While Loop: Use when a loop is based on a more conditional length
+
+// Array === [] -> Collection of ORDERED data
+
+// ['h', 'e', 'l', 'l', 'o']
+
+// let myArr = [5, true, [1,2,3], 'hello', NaN, undefined, , 'hello'];
+
+// console.log(myArr[myArr.length - 1]);
+
+// for(let banana = 0; banana < myArr.length; banana += 1){
+//     console.log(myArr[banana]);
+// };
+
+// console.log(myArr.indexOf('hello'));
+
+// let a = [1,2,3];
+// let b = [4,5,6];
+
+// console.log(a.concat(b));
+// console.log('hello' + b);
+
+
+// ------------------- PST --------------------
+
+// statement that says "do i run this code"
+
+
+// console.log('hello');
+// console.log('world');
+
+
+// let myName = 'Kate';
+
+// if(myName === 'William'){
+//     console.log('Hello William')
+// } else {
+//     console.log('Hello person!')
+// };
+
+
+// if(){
+
+// };
+
+// if(){};
+
+// if(condition){
+
+// }
+
+// if(){
+
+// }
+
+// if(){
+
+// }
+
+
+// while / for
+
+//0 -- myName.length - 1
+// let myName = 'William'
+// let i = 0;
+
+// while(i < myName.length){
+//     let char = myName[i];
+//     console.log(i, ': ', char);
+//     i = i + 1;
+// };
+
+
+//starting -- condition
+//stopping point --condition
+//step
+
+// let val = true;
+// let count = 0;
+
+// while(val){
+//     console.log(count);
+    
+//     if(count === 5){
+//         val = false;
+//     }
+    
+//     count = count + 1;
+// }
+
+
+//for(start, stop, step){}
+//for(i = 0) -> do not do this
+
+// let word = 'banana';
+
+// for(let i = 0; i < word.length; i = i + 1){
+//     let char = word[i];
+//     console.log(char);
+// };
+
+
+// while: loop based on some form of unknown or external condition
+// for: loop based on predefined conditions
+
+
+// Arrays are ORDERED
+let myArr = [1,2,3, 'hello', [5,6,7], NaN, undefined, 7];
+
+let myStr = 'hello';
+// let myStr2 = ['h', 'e', 'l', 'l', 'o'];
+
+// console.log(myArr[myArr.length - 1]);
+
+// for(let i = 0; i < myArr.length; i = i + 1){
+//     let ele = myArr[i];
+//     console.log(ele);
+// }
+
+// console.log(myArr.length);
+
+
+// let a = [1,2,3];
+// let b = [4,5,6];
+
+// // console.log('hello' + b);
+// let c = a.concat(b);
+// console.log(a, b);
+
+// for()
+
+/*
+
+*/
