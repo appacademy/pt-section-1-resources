a23d7358f01ccd397ee2983f4e6b2bf65fce8b01
Zaviar Brown <brown.zaviar@gmail.com>
2024-04-24 17:13:21 -0700

    Console logs are actually the best thing ever and you should always use them

--- 01-week/3-day/EST/scratch.js
+++ 01-week/3-day/EST/scratch.js
@@ -21,3 +21,23 @@ function myExample() {
 // console.log(myNumber);
 // console.log(myNumber);
 // console.log(myNumber);
+
+//! --------------------------------------------------------------------
+//*                       console.log for loop example                                             
+//! --------------------------------------------------------------------
+
+// let num = 3;
+
+// for (let i = 0; i < 5; i++) {
+//     console.log('----------A new loop as begun!---------------');
+//     console.log('Loop with i =', i);
+//     console.log('Num before adding', num);
+
+//     num = i + num;
+
+//     console.log('Num after adding', num);
+
+//     console.log('----- End of this loop :) -------');
+// }
+
+// console.log(num);
