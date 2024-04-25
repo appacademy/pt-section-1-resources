db9de3a5b533692e57bb38b433d5324f970f157e
Zaviar Brown <brown.zaviar@gmail.com>
2024-04-24 21:36:32 -0700

    I'm british now

--- 01-week/3-day/PST/scratch.js
+++ 01-week/3-day/PST/scratch.js
@@ -1,1 +1,84 @@
-console.log('Hey');
+// console.log('Hey');
+
+// function myExample() {
+//     return 3;
+// }
+
+// function myBetterExample() {
+//     // return 42;
+//     return 32;
+// }
+
+// let myNum = 0.1;
+// let myOtherNum = 0.2
+// let finalNum = myNum + myOtherNum
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log("This is the REAL finalNum", finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+// console.log(finalNum);
+
+// let myNum = 0.1;
+// let myOtherNum = 0.2;
+// let finalNum = myNum + myOtherNum;
+
+// if (finalNum === 0.3) {
+//     console.log("YUUUUP")
+// } else {
+//     console.log("who da what now?")
+// }
+
+// console.log(finalNum)
+
+//! --------------------------------------------------------------------
+//*                           Crazy for loop example
+//! --------------------------------------------------------------------
+
+// let counter = 3;
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(
+//         '-------------------------A new loop just began!----------------------------'
+//     );
+//     console.log('this loop is i =', i);
+//     console.log('Counter before the math', counter);
+
+//     console.log('running', i, '+ 5 *', counter);
+//     counter = (i + 6) * counter;
+
+//     console.log('Counter after the math', counter);
+// }
+
+// console.log('Counter after the loop', counter);
+
+//! --------------------------------------------------------------------
+//*                           Sad no console.log example
+//! --------------------------------------------------------------------
+
+// let counter = 3;
+
+// for (let i = 0; i < 10; i++) {
+//     counter = (i + 6) * counter;
+// }
+
+// console.log(counter);
