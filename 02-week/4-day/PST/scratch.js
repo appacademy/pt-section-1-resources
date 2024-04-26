e2cbbb9ff13f98f6708a83c70d4edf96fd0a71d5
Zaviar Brown <brown.zaviar@gmail.com>
2024-04-25 22:55:22 -0700

    Bonus: For loop => While loop

--- 02-week/4-day/PST/scratch.js
+++ 02-week/4-day/PST/scratch.js
@@ -101,15 +101,110 @@
 // year--;
 // console.log(year); // 3004
 
-let preFix = 10;
-console.log(preFix); // 10
-console.log(++preFix); // 10
-// console.log(++preFix / 2); // 11
-// console.log(1 + preFix / 2); // 11
-console.log(preFix); // 11
+// let preFix = 10;
+// console.log(preFix); // 10
+// console.log(++preFix); // 10
+// // console.log(++preFix / 2); // 11
+// // console.log(1 + preFix / 2); // 11
+// console.log(preFix); // 11
 
 // let banana = 10;
 // console.log(banana); // 10
 // // banana++
 // console.log(banana++); // 10
 // console.log(banana); // 11
+
+//! --------------------------------------------------------------------
+//*                              Strings
+//! --------------------------------------------------------------------
+
+// let firstName = 'Zaviar';
+// let lastName = 'Brown';
+// console.log('Zaviar'.length);
+// console.log(firstName.length);
+// console.log(firstName.indexOf('q'));
+// console.log(firstName[-1]);
+
+// console.log(firstName[-1])
+// console.log(firstName.at(-1));
+
+// console.log(firstName, lastName)
+// console.log(firstName + lastName);
+// console.log(firstName + " " + lastName);
+// let word = 'cooler';
+// let word2 = 'than';
+// let myNum = 100;
+// function myFunc() {
+//     console.log('Heyoooo');
+
+//     return 'the return value';
+// }
+
+// console.log(myFunc);
+// console.log(myFunc());
+
+// console.log(`${firstName} ${'Brown'}`);
+// console.log("${firstName} ${'Brown'}");
+// console.log(`This is a normal string like any other type`);
+// console.log(`This is a ${word} string ${myFunc()} any other type`);
+
+//! --------------------------------------------------------------------
+//*                          Loop Sneak Peak
+//! --------------------------------------------------------------------
+
+//? Create a for loop that increments a counter by 5 until counter = 50
+
+let counter = 0;
+
+for (let i = 0; i < 10; i += 10 ) {
+    console.log(
+        'New loop began with i =',
+        i,
+        'and counter starting at',
+        counter
+    );
+    console.log('Just added 5 😎');
+
+    counter += 5;
+    console.log({ counter });
+    i++;
+    console.log('Loop ended, i is now', i);
+}
+
+console.log(counter);
+
+//? Create a while loop that increments a counter by 5 until counter = 50
+
+// let counter = 0;
+
+// while (counter < 50) {
+//     counter += 5;
+// }
+
+// console.log(counter);
+
+//??? Translation!
+
+// let counter = 0;
+// let i = 0;
+// for (; i < 10; ) {
+//     counter += 5;
+//     i++;
+// }
+
+// console.log(counter);
+
+// let counter = 0;
+// let i = 0;
+// while ( i < 10 ) {
+//     counter += 5;
+//     i++;
+// }
+
+// console.log(counter);
+
+//? When to while vs When to for
+
+// Q: Do I know how many times I should be looping?
+// A: Yes === For Loop
+// A: No === While loop
