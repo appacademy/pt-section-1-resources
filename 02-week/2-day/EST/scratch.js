1931df0773cb588b3f42046ecc4c782d5b651347
Zaviar Brown <brown.zaviar@gmail.com>
2024-04-30 15:55:13 -0700

    Mutability short lecture notes

---
+++ 02-week/2-day/EST/scratch.js
@@ -0,0 +1,21 @@
+// let str = 'abc';
+// str += 'd'; // str = str + 'd';
+// console.log(str); // 'abcd'
+
+// let num = 5;
+
+// num = num + 10;
+
+// console.log(num);
+
+let arr = ['a', 'b', 'c'];
+
+console.log(arr);
+
+arr[1] = 'x';
+
+console.log(arr); // ["a", "x", "c"];
+
+// arr.pop();
+
+// console.log(arr);
