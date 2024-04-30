4078d29352aa2c923a213be3cd77be8d96f3b952
Zaviar Brown <brown.zaviar@gmail.com>
2024-04-30 15:54:31 -0700

    Array problem 12 solution

--- 02-week/1-day/EST/arrays-practice/problems/12-three-increasing.js
+++ 02-week/1-day/EST/arrays-practice/problems/12-three-increasing.js
@@ -4,13 +4,72 @@ array contains three consecutive numbers in consecutive increasing order, like
 7, 8, 9.
 */
 
-// Your code here 
+// function threeIncreasing(arr) {
+//     let isIncreasing = false;
+//     let counter = 0;
 
+//     console.log(arr);
 
-// console.log(threeIncreasing([3, 2, 11, 12, 13, 2, 4]));     // true
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log('******** New Loop Just Began! ********');
+
+//         let currentNumber = arr[i];
+//         let nextNumber = arr[i + 1];
+
+//         console.log('currNum & nextNum', currentNumber, nextNumber);
+
+//         if (currentNumber + 1 === nextNumber) {
+//             counter++;
+//             console.log('counter just went up!', counter);
+//         } else {
+//             counter = 0;
+//             console.log('not consecutive, so counter resets', counter);
+//         }
+
+//         if (counter === 2) {
+//             isIncreasing = true;
+//         }
+//     }
+
+//     return isIncreasing;
+// }
+
+function threeIncreasing(arr) {
+    let isIncreasing = false;
+    let counter = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNumber = arr[i];
+        let nextNumber = arr[i + 1];
+
+        if (currentNumber + 1 === nextNumber) {
+            counter++;
+        } else {
+            counter = 0;
+        }
+
+        if (counter === 2) {
+            isIncreasing = true;
+        }
+    }
+
+    return isIncreasing;
+}
+
+// function threeIncreasing(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] + 1 === arr[i + 1] && arr[i] + 2 === arr[i + 2]) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+console.log(threeIncreasing([3, 2, 11, 12, 13, 2, 4])); // true
 // console.log(threeIncreasing([2, 7, 8, 9]));                 // true
 // console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6]));        // false
-// console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8]));        // false
+// console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8])); // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = threeIncreasing;
