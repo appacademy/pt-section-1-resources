caf42c7b1f1ef48ff88e3b7ad8200f622c9f451b
Zaviar Brown <brown.zaviar@gmail.com>
2024-05-04 13:46:09 -0700

    Review problems

---
+++ 02-week/concept-review/10-maximum-value.js
@@ -0,0 +1,11 @@
+/*
+Define a function called maxVal that takes in an array of numbers as a
+parameter. The function should return the largest number of the array. If the
+array is empty, the function should return null.
+*/
+
+// Your code here
+
+// console.log(maxVal([4, 6, 3, 5, 42, 4])); // 42
+// console.log(maxVal([-2, -3, -7, 3 ]));    // 3
+// console.log(maxVal([]));                  // null
---
+++ 02-week/concept-review/2-repeat-name.js
@@ -0,0 +1,17 @@
+/*
+Assign the variables repeatDaniel, repeatMark, and repeatBeyonce to values
+by calling the repeatName(name) function so that the console.logs will
+print out the expected values below.
+*/
+
+function repeatName(name) {
+    return name + " " + name;
+}
+
+let repeatDaniel;
+let repeatMark;
+let repeatBeyonce;
+
+console.log(repeatDaniel);  // -> "Daniel Daniel"
+console.log(repeatMark);    // -> "Mark Mark"
+console.log(repeatBeyonce); // -> "!!!Beyonce Beyonce!!!"
