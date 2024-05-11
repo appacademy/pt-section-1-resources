d7bd6020ef25939bea926537a2ff0123660524b9
gisales92 <isales.greg@gmail.com>
2024-05-11 16:28:14 -0700

    adding Array.isArray example

--- 03-week/5-day/scratch.js
+++ 03-week/5-day/scratch.js
@@ -26,11 +26,29 @@ const ppl = [{first: "Greg", last: "Isales"}, {first: "Bob", last: "bobbington"}
     const numbers = [1, 2, 3, 4, 5];
 
     const sum = numbers.reduce((acc, el) => {
-        console.log("Acc:", acc, "El:", el)
+        // console.log("Acc:", acc, "El:", el)
         let sum = acc + el;
         return sum
     }, 0)
 
-    console.log(sum);
+    // console.log(sum);
 
 // console.log("Mapped:", mappedNumbers);
+
+
+const num = 1;
+const str = "Greg";
+const bool = true;
+const und = undefined;
+const arr = [1, 2, 3, 4];
+const obj = {someKey: "some value"};
+
+console.log("Number:", typeof num);
+console.log("String:", typeof str);
+console.log("Boolean:", typeof bool);
+console.log("Undefined:", typeof und);
+console.log("Array:", typeof arr);
+console.log("Object:", typeof obj);
+
+console.log("Is Array:", Array.isArray(arr))
+console.log("Is Array:", Array.isArray(obj))
