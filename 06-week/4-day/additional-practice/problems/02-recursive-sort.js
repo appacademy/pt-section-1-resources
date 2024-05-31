d1101a04c3db11cad7a283049aa0df2b2bf2b18c
gisales92 <isales.greg@gmail.com>
2024-05-30 22:29:33 -0700

    adding 02 solutions

--- 06-week/4-day/additional-practice/problems/02-recursive-sort.js
+++ 06-week/4-day/additional-practice/problems/02-recursive-sort.js
@@ -10,12 +10,45 @@
 */
 
 function sort(nums) {
-    // Your code here 
+  //base case
+  if (nums.length <= 1) return nums;
+
+  // Finding the max
+  let max = nums[0];
+  let maxInd = 0;
+
+  nums.forEach((num, i) => {
+    if (num > max) {
+      max = num;
+      maxInd = i;
+    }
+  });
+
+  nums.splice(maxInd, 1);
+  return [...sort(nums), max];
+
+  // Finding the min
+  //   let min = nums[0];
+  //   let minInd = 0;
+  //   nums.forEach((num, i) => {
+  //     if (num < min) {
+  //       min = num;
+  //       minInd = i;
+  //     }
+  //   });
+
+  //   nums.splice(minInd, 1);
+  //   return [min, ...sort(nums)];
 }
+
+console.log(sort([])); // prints []
+console.log(sort([9])); // prints [9]
+console.log(sort([5, 4, 3, 2, 1])); // prints [1, 2, 3, 4, 5]
+console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-    module.exports = sort;
+  module.exports = sort;
 } catch (e) {
-    module.exports = null;
+  module.exports = null;
 }
