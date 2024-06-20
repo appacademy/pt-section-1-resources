65347e4ba443ece23f7a10c160e8c947ee4cfa2f
gisales92 <isales.greg@gmail.com>
2024-06-20 16:18:42 -0700

    adding lecture notes

---
+++ 09-week/4-day/scratch.js
@@ -0,0 +1,16 @@
+// console.log(parseInt("0x13", 16))
+// console.log(parseInt("1111", 2))
+// const fifteen = Number(15);
+// console.log(fifteen.toString(16));
+// const dog = "Yoshi";
+
+// const codes = [];
+// for (let i = 0; i < dog.length; i++) {
+//     let code = dog.charCodeAt(i);
+//     codes.push(code);
+// }
+// console.log(codes)
+
+// let rebuilt = "";
+// codes.forEach(code => rebuilt += String.fromCharCode(code));
+// console.log(rebuilt)
