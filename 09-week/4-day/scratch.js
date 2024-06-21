ce9dc4ab195e2422830935f6eb8e7d6dd83d4e91
gisales92 <isales.greg@gmail.com>
2024-06-20 22:01:33 -0700

    adding walkthru

--- 09-week/4-day/scratch.js
+++ 09-week/4-day/scratch.js
@@ -14,3 +14,27 @@
 // let rebuilt = "";
 // codes.forEach(code => rebuilt += String.fromCharCode(code));
 // console.log(rebuilt)
+
+// ----------------------- PST --------------------------------
+
+// Non-base 10 to base 10
+const binary = "111010101101";
+console.log(parseInt(binary, 2));
+const hex1 = "0x328761AADE";
+console.log(parseInt(hex1))
+
+//base 10 to non-base 10
+// const num1 = Number(100);
+// console.log("0b" + num1.toString(2))
+// console.log(num1.toString(16))
+
+// console.log(String.fromCharCode(78, 100, 66, 88))
+
+// const dog = "Yoshi";
+
+// const codes = [];
+// for (let i = 0; i < dog.length; i++) {
+//     codes.push(dog.charCodeAt(i))
+// };
+
+// console.log(codes)
