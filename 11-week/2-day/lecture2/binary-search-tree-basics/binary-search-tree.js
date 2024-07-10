5e77cced68cb23a7700113c186b5c928e5cbaba0
gisales92 <isales.greg@gmail.com>
2024-07-09 20:51:28 -0700

    adding lec 2 code

--- 11-week/2-day/lecture2/binary-search-tree-basics/binary-search-tree.js
+++ 11-week/2-day/lecture2/binary-search-tree-basics/binary-search-tree.js
@@ -8,42 +8,105 @@ class TreeNode {
 }
 
 class BinarySearchTree {
-
   constructor() {
-    // Your code here 
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
-    // Your code here 
+  insert(val, currentNode = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = new TreeNode(val);
+      } else {
+        this.insert(val, currentNode.left);
+      }
+    } else {
+      if (!currentNode.right) {
+        currentNode.right = new TreeNode(val);
+      } else {
+        this.insert(val, currentNode.right);
+      }
+    }
   }
 
   search(val) {
-    // Your code here 
-  }
+    let curr = this.root;
 
+    while (curr) {
+      if (curr.val === val) return true;
+      if (val > curr.val) {
+        curr = curr.right;
+      } else {
+        curr = curr.left;
+      }
+    }
+    return false;
+  }
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here 
-  }
+    if (!currentNode) return;
 
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
+  }
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here 
-  }
+    if (!currentNode) return;
 
+    this.inOrderTraversal(currentNode.left);
+    console.log(currentNode.val);
+    this.inOrderTraversal(currentNode.right);
+  }
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here 
+    if (!currentNode) return;
+
+    this.postOrderTraversal(currentNode.left);
+    this.postOrderTraversal(currentNode.right);
+    console.log(currentNode.val);
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
-    // Your code here 
+    // initialize a queue with the root node
+    const queue = [this.root];
+
+    // while the queue is not empty
+    while (queue.length) {
+      // print and remove first node in queue
+      const first = queue.shift();
+      console.log(first.val);
+      // if the node has a left node
+      // push the left node on the back of the queue
+      if (first.left) queue.push(first.left);
+      // if the node has a right node
+      // push the right node on the back of the queue
+      if (first.right) queue.push(first.right);
+    }
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
-    // Your code here 
+    // initialize a stack with the root node
+    const stack = [this.root];
+
+    // while the stack is not empty
+    while (stack.length) {
+      // print and remove first node in stack
+      const first = stack.pop();
+      console.log(first.val);
+      // if the node has a left node
+      if (first.left) stack.push(first.left);
+      // push the left node on the back of the stack
+      // if the node has a right node
+      // push the right node on the back of the stack
+      if (first.right) stack.push(first.right);
+    }
   }
 }
 
--- 11-week/2-day/lecture2/practice-binary-search/binary-search.js
+++ 11-week/2-day/lecture2/practice-binary-search/binary-search.js
@@ -1,26 +1,34 @@
 function linearSearch (arr, target) {
 
-  // Can you solve this in one line?
+  return arr.indexOf(target);
 
 };
 
 function binarySearch(arr, target) {
+  
 
   // Set integers pointing to the high and low range of possible indices
+  let low = 0;
+  let high = arr.length - 1;
 
   // While high and low indices do not overlap...
-
+  while (low <= high) {
     // Find the midpoint between high and low indices
-
+    let mid = Math.floor((low + high) / 2);
     // Compare the target value to the midpoint value
 
     // If the target equals the midpoint...
+    if (arr[mid] === target) return mid;
       // Return the midpoint index
     // If the target is higher than the midpoint...
+    if (target > arr[mid]) low = mid + 1
       // Move the low pointer to midpoint + 1
+    else if (target < arr[mid]) high = mid - 1;
     // If the target is less than the midpoint...
       // Move the high pointer to midpoint - 1
+  }
 
+  return -1;
   // Return -1 if the loop exits with overlapping pointers
 
 }
