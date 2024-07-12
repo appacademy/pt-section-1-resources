acc93f9dae38972aad9bea34a13dcf7f0e379264
gisales92 <isales.greg@gmail.com>
2024-07-11 20:06:22 -0700

    adding lecture 2

--- 11-week/3-day/lecture2/binary-search-tree/tree-practice.js
+++ 11-week/3-day/lecture2/binary-search-tree/tree-practice.js
@@ -67,28 +67,116 @@ function countNodes(rootNode) {
 }
 
 function getParentNode(rootNode, target) {
-  // Your code here
+  if (rootNode.val === target) return null;
+
+  let stack = [rootNode];
+
+  while (stack.length) {
+    let curr = stack.pop();
+
+    if (
+      (curr.left && curr.left.val === target) ||
+      (curr.right && curr.right.val === target)
+    ) {
+      return curr;
+    }
+    if (curr.left) stack.push(curr.left);
+    if (curr.right) stack.push(curr.right);
+  }
+  return;
 }
 
 function inOrderPredecessor(rootNode, target) {
-  // Your code here
+  // RECURSIVE
+  // let pred = null;
+  // let found = false;
+
+  // const inOrderTraversal = (node) => {
+
+  //   if (node.left) inOrderTraversal(node.left);
+  //   if (node.val === target) {
+  //     found = true
+  //   }
+  //   if (!found) pred = node.val;
+  //   if (!found && node.right) inOrderTraversal(node.right);
+  // }
+
+  // inOrderTraversal(rootNode);
+  // return pred;
+
+  // // ITERATIVE
+  let curr = rootNode;
+  let stack = [];
+  let pred = null;
+
+  while (true) {
+    if (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    } else if (!curr && stack.length) {
+      curr = stack.pop();
+
+      if (curr.val === target) {
+        if (!pred) return null;
+        return pred.val;
+      }
+      pred = curr;
+      curr = curr.right;
+    } else {
+      break;
+    }
+  }
 }
 
 function deleteNodeBST(rootNode, target) {
   // Do a traversal to find the node. Keep track of the parent
   // Undefined if the target cannot be found
+  let parent = getParentNode(rootNode, target);
+
+  if (parent === undefined) return;
+
+  let targetNode;
+  let isLeftChild = false;
+  if (!parent) targetNode = rootNode;
+  else if (parent.left && parent.left.val === target) {
+    targetNode = parent.left;
+    isLeftChild = true;
+  } else {
+    targetNode = parent.right;
+  }
   // Set target based on parent
   // Case 0: Zero children and no parent:
   //   return null
+  if (!parent && !targetNode.left && !targetNode.right) {
+    return null;
+  }
   // Case 1: Zero children:
   //   Set the parent that points to it to null
+  else if (!targetNode.left && !targetNode.right) {
+    if (isLeftChild) parent.left = null;
+    else parent.right = null;
+  }
   // Case 2: Two children:
-  //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side,
-  //  or the right most child on its left side.
-  //  Then delete the child that it was replaced with.
+  else if (targetNode.left && targetNode.right) {
+    //  Set the value to its in-order predecessor, then delete the predecessor
+    let pred = inOrderPredecessor(rootNode, target);
+    //  Replace target node with the left most child on its right side,
+    deleteNodeBST(rootNode, pred);
+    //  or the right most child on its left side.
+    targetNode.val = pred;
+    //  Then delete the child that it was replaced with.
+  }
   // Case 3: One child:
   //   Make the parent point to the child
+  else {
+    if (targetNode.left) {
+      if (isLeftChild) parent.left = targetNode.left
+      else parent.right = targetNode.left
+    } else {
+      if (isLeftChild) parent.left = targetNode.right
+      else parent.right = targetNode.right
+    }
+  }
 }
 
 module.exports = {
